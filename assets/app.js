8e8a1bfd0118bf3cb78a18013c3be10bc22271c2
Dmitriy Kirillov <dkirillov1981@gmail.com>
2023-11-20 16:18:21 +0300

    Update name field by click on result item

--- assets/app.js
+++ assets/app.js
@@ -9,3 +9,44 @@
 // window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle';
 import './app.scss';
+
+let nameSuggestion = document.getElementById('name_suggestion');
+if (nameSuggestion) {
+    function addSearchResultListeners() {
+        searchResult.querySelectorAll('td a').forEach(function (node) {
+            node.addEventListener('click', function (e) {
+                e.preventDefault();
+                let nameField = document.getElementById('MAKTX');
+                nameField.value = node.innerHTML;
+                searchResult.innerHTML = '';
+            });
+        });
+    }
+    let nameSuggestionStatus = false;
+    nameSuggestion.addEventListener('input', function (e) {
+        nameSuggestionStatus = nameSuggestion.checked;
+        if (!nameSuggestionStatus) {
+            searchResult.innerHTML = '';
+        }
+    });
+    let searchResult = document.getElementById('search-result');
+    let searchField = document.getElementById('MAKTX');
+    searchField.addEventListener('input', function (e) {
+        if (nameSuggestionStatus) {
+            if (searchField.value) {
+                let url = encodeURI('/search?search=' + searchField.value);
+                fetch(url)
+                    .then(function (response) {
+                        return response.text()
+                    })
+                    .then(function (html) {
+                        searchResult.innerHTML = html;
+                        addSearchResultListeners();
+                    });
+            } else {
+                searchResult.innerHTML = '';
+            }
+        }
+
+    });
+}
